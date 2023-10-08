6c534ee95fa45798f835f8936a3d03057cc1ded3
Yash Gupta <yashnitagartala8@gmail.com>
2023-10-08 13:21:03 +0530

    understanding getter and setter

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <h2>Fullname-{{ firstName }} {{ secondName }}</h2>
-  <h2>Computed Fullname-{{ fullname }}</h2>
+  <!-- <h2>Computed Fullname-{{ fullname }}</h2> -->
+  <button @click="changefullName">ChangeFull Name</button>
   <h2>
     Total is
     {{ total }}
@@ -8,6 +9,10 @@
   <button @click="items.push({ id: 4, title: 'mac', price: 400 })">Add items</button>
   <h2>Total from Method is -{{ getTotal() }}</h2>
   <input type="text" v-model="country" />
+  <template v-for="item in items" :key="item.id">
+    <div v-if="item.price > 100">{{ item.title }}</div>
+  </template>
+  <h2 v-for="item in expensive" :key="item.id">{{ item.price }} {{ item.title }}</h2>
 </template>
 
 <script>
@@ -31,15 +36,28 @@ export default {
     getTotal() {
       console.log('total in method')
       return this.items.reduce((total, curr) => total + curr.price, 0)
+    },
+    changefullName() {
+      return (this.fullname = 'Angela White')
     }
   },
   computed: {
-    fullname() {
-      return `${this.firstName} ${this.secondName}`
+    fullname: {
+      get() {
+        return `${this.firstName} ${this.secondName}`
+      },
+      set(value) {
+        const name = value.split(',')
+        this.firstName = name[0]
+        this.secondName = name[1]
+      }
     },
     total() {
       console.log('total in computed')
       return this.items.reduce((total, curr) => total + curr.price, 0)
+    },
+    expensive() {
+      return this.items.filter((item) => item.price > 100)
     }
   }
 }
