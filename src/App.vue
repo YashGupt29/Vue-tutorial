4a470787ddecef8e75f01a1ca2a55f33ba145dca
Yash Gupta <yashnitagartala8@gmail.com>
2023-10-08 12:44:41 +0530

    understanding difference between computed and methods property

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <h2>{{ name }}</h2>
-  <!-- v-once directive is used to not re-render the component as useMemo in react -->
-  <button @click="name = 'lund'" v-once>Change Name</button>
-  <!-- v-pre directive is used to not to compile the javascript -->
-  <h2 v-pre>{{ name }}</h2>
+  <h2>Fullname-{{ firstName }} {{ secondName }}</h2>
+  <h2>Computed Fullname-{{ fullname }}</h2>
+  <h2>
+    Total is
+    {{ total }}
+  </h2>
+  <button @click="items.push({ id: 4, title: 'mac', price: 400 })">Add items</button>
+  <h2>Total from Method is -{{ getTotal() }}</h2>
+  <input type="text" v-model="country" />
 </template>
 
 <script>
@@ -11,9 +15,32 @@ export default {
   name: 'App',
   data() {
     return {
-      name: 'yash'
+      firstName: 'Yash',
+      secondName: 'Gupta',
+      items: [
+        { id: '1', title: 'TV', price: 100 },
+        { id: '2', title: 'Mobile', price: 200 },
+        { id: '3', title: 'Laptop', price: 300 }
+      ],
+      country: ''
     }
   },
-  methods: {}
+  // difference between computed and method property is that computed property is cached and not recalculated again but method is recalculated
+  // hence improve the performance
+  methods: {
+    getTotal() {
+      console.log('total in method')
+      return this.items.reduce((total, curr) => total + curr.price, 0)
+    }
+  },
+  computed: {
+    fullname() {
+      return `${this.firstName} ${this.secondName}`
+    },
+    total() {
+      console.log('total in computed')
+      return this.items.reduce((total, curr) => total + curr.price, 0)
+    }
+  }
 }
 </script>
